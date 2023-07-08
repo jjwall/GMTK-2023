91da0788c7f28aa499c8788353816108e00057da
jjwall <jacob.wallace@utexas.edu>
2023-07-07 22:07:37 -0500

    type updates

--- objects/field_unit/field_unit.gd
+++ objects/field_unit/field_unit.gd
@@ -12,12 +12,7 @@ const scissors_texture = preload("res://assets/textures/scissors_emoji.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if type == 'rock':
-		$Sprite2D.texture = rock_texture
-	elif type == 'paper':
-		$Sprite2D.texture = paper_texture
-	else:
-		$Sprite2D.texture = scissors_texture
+	process_type_update()
 
 
 func _physics_process(delta):
@@ -25,3 +20,27 @@ func _physics_process(delta):
 	velocity.y = move_toward(velocity.y, random_y_pos, SPEED)
 
 	move_and_slide()
+	var last_slide_collision = get_last_slide_collision()
+	
+	if last_slide_collision:
+		if last_slide_collision.get_collider().is_in_group("field_units"):
+			if type == 'rock' && last_slide_collision.get_collider().type == 'paper':
+				type = 'paper'
+				process_type_update()
+			if type == 'paper' && last_slide_collision.get_collider().type == 'scissors':
+				type = 'scissors'
+				process_type_update()
+			if type == 'scissors' && last_slide_collision.get_collider().type == 'rock':
+				type = 'rock'
+				process_type_update()
+	
+func _process(delta):
+	pass
+
+func process_type_update():
+	if type == 'rock':
+		$Sprite2D.texture = rock_texture
+	elif type == 'paper':
+		$Sprite2D.texture = paper_texture
+	else:
+		$Sprite2D.texture = scissors_texture
