a87547af003bcc5e434a64c75110b26122495c3d
jjwall <jacob.wallace@utexas.edu>
2023-07-07 21:20:59 -0500

    field unit can be instantiated as different types

--- objects/field_unit/field_unit.gd
+++ objects/field_unit/field_unit.gd
@@ -1,8 +1,24 @@
 extends CharacterBody2D
 
+@export var type = 'rock' # | 'paper' | 'scissors'
+
 const SPEED = 300.0
 var random_x_pos = randf_range(-200, 200)
 var random_y_pos = randf_range(-200, 200)
+
+const paper = preload("res://assets/textures/paper_emoji.png")
+const rock = preload("res://assets/textures/rock_emoji.png")
+const scissors = preload("res://assets/textures/scissors_emoji.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if type == 'rock':
+		$Sprite2D.texture = rock
+	elif type == 'paper':
+		$Sprite2D.texture = paper
+	else:
+		$Sprite2D.texture = scissors
+
 
 func _physics_process(delta):
 	velocity.x = move_toward(velocity.x, random_x_pos, SPEED)
