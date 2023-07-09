5052f9ffacbac21ae4cc896eb02e266514140a3a
WiredOverload <mike.everywhere@gmail.com>
2023-07-09 02:39:57 -0500

    finished, this is basically the gif

--- objects/field_unit/field_unit.gd
+++ objects/field_unit/field_unit.gd
@@ -2,7 +2,9 @@ extends Area2D
 
 @export var type = 'rock' # | 'paper' | 'scissors'
 @export var field_units_group: Node2D = null
-@export var trigger_distance = 100
+
+@export var imprecision = 4 #increase this number for more performance
+var check_num = 0
 
 var SPEED = 75
 var target : Area2D = null
@@ -13,21 +15,24 @@ const rock_texture = preload("res://assets/textures/rock_emoji.png")
 const scissors_texture = preload("res://assets/textures/scissors_emoji.png")
 
 var sprite : Sprite2D
+var target_search : Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SPEED = randf_range(75, 125)
 	sprite = $Sprite2D
+	target_search = $TargetSearchArea
 	process_type_update()
-	aimless_direction = Vector2(randf(), randf())
+	aimless_direction = Vector2((randf() * 2) - 1, (randf() * 2) - 1)
 
-func _physics_process(delta):	
-#	if !target:
-	locate_target()
+func _physics_process(delta):
+	check_num += 1
+	if check_num == imprecision:
+		locate_target()
+		check_num = 0
 	
-	var direction = global_position.direction_to(target.position)
-	
-	if (position - target.position).length() < trigger_distance:
+	if target:
+		var direction = global_position.direction_to(target.position)
 		position += direction * SPEED * delta
 	else:
 		position += aimless_direction * SPEED * delta
@@ -42,11 +47,14 @@ func _physics_process(delta):
 		position.y = 720
 
 func locate_target():
-	var children = field_units_group.get_children()
+#	var children = field_units_group.get_children()
+	var children = target_search.get_overlapping_areas()
+	if children.size() < 1:
+		return
 	var min_distance = 999999
-	var min_node : Area2D = target
+	var min_node : Area2D = null
 	var current_distance = 9999999
-	for i in range(0, field_units_group.get_child_count() - 1):
+	for i in range(0, children.size() - 1):
 		if children[i].type == 'rock':
 			if self.type == 'paper':
 				current_distance = position.distance_squared_to(children[i].position)
@@ -66,17 +74,6 @@ func locate_target():
 					min_distance = current_distance
 					min_node = children[i]
 	target = min_node
-#	children.shuffle()
-#	for i in range(0, field_units_group.get_child_count() - 1):
-#		if children[i].type == 'rock':
-#			if self.type == 'paper':
-#				target = children[i]
-#		if children[i].type == 'paper':
-#			if self.type == 'scissors':
-#				target = children[i]
-#		if children[i].type == 'scissors':
-#			if self.type == 'rock':
-#				target = children[i]
 
 func process_collision(colliding_entity: Area2D):
 	if type == 'rock' && colliding_entity.type == 'paper':
@@ -90,6 +87,7 @@ func process_collision(colliding_entity: Area2D):
 		process_type_update()
 
 func process_type_update():
+	target = null
 	locate_target()
 	if type == 'rock':
 		sprite.texture = rock_texture
