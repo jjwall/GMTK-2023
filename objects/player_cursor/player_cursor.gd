ecfffe5ee753007c0f39bb655f9ad1c92dfa95c3
jjwall <jacob.wallace@utexas.edu>
2023-07-22 19:08:38 -0500

    clean up player cursor script

--- objects/player_cursor/player_cursor.gd
+++ objects/player_cursor/player_cursor.gd
@@ -1,23 +1,17 @@
 extends RigidBody2D
 
-# Store the points of the line
+# Notes:
+# Player_Cursor could be a regular node
+# That dynamically appends a static or rigidbody2d
+# when the line in being drawn
+
 var line_points = PackedVector2Array()
-# Reference to the CollisionPolygon2D node
-var collision_polygon
-
 var collision_children: Array[CollisionShape2D] = []
-
-# Reference to the Line2D node
-var line_node
+var line_node: Line2D
 
 func _ready():
 	# Get the reference to the Line2D and CollisionPolygon2D nodes
 	line_node = get_node("Line2D")
-	collision_polygon = get_node("CollisionShape2D")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -33,55 +27,18 @@ func _input(event):
 				collision_line.b = line_points[line_points.size() - 1]
 				var shape = CollisionShape2D.new()
 				shape.shape = collision_line
-	#			collision_polygon.shape = CircleShape2D.new()
-	#			collision_polygon.position = event.position
 				self.add_child(shape)
 				collision_children.append(shape)
-			# Update the Line2D node with the new points
-			# Update the CollisionPolygon2D node with the new points
-#			collision_polygon.polygon = line_points
-#			var shape = ConvexPolygonShape2D.new()
-#			shape.set_points(line_points)
-#			collision_polygon.shape = shape
 		
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		# Reset the line_points array when the left mouse button is pressed
 		line_points.clear()
 		line_node.points = line_points
-		delete_children(collision_children)
-#		collision_polygon.polygon = line_points
+		delete_collision_children()
 
-func delete_children(node: Array[CollisionShape2D]):
-	for n in range(node.size()):
-		node[n].queue_free()
-#		node.remove_at(n)
+func delete_collision_children():
+	for n in range(collision_children.size()):
+		collision_children[n].queue_free()
+		
 	collision_children = []
 
-
-#func _input(event):
-#	# Mouse in viewport coordinates.
-#	if event is InputEventMouseButton:
-#		print("Mouse Click/Unclick at: ", event.position)
-#	elif event is InputEventMouseMotion:
-#		print("Mouse Motion at: ", event.position)
-	# Print the size of the viewport.
-#	print("Viewport Resolution is: ", get_viewport_rect().size)
-
-func _on_body_entered(body):
-	if body.is_in_group("field_units"):
-		print("yo")
-		# Calculate the point of intersection
-#		var intersection = get_collision_point()
-#
-#		# Calculate the normal of the collision (perpendicular to the line)
-#		var normal = get_collision_normal()
-#
-#		# Calculate the direction from the intersection point to the object's position
-#		var to_intersection = intersection - global_position
-#
-#		# Project the direction onto the normal to get the component that should be removed
-#		var projection = to_intersection.project(normal)
-#
-#		# Calculate the new position of the object to prevent it from crossing the line
-#		global_position += projection
-#		velocity = Vector2.ZERO # Stop the object's movement
