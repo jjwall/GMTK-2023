80538126e2d174e7adefcb548ed725b7217cb81f
jjwall <jacob.wallace@utexas.edu>
2023-07-24 21:39:17 -0500

    reducing reduntancy in spawner code with create_field_unit function

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -22,34 +22,27 @@ var rand_paper_y_max = 1920
 var field_unit_scene = preload("res://objects/field_unit/field_unit.tscn")
 
 func _ready():
-	spawn_units()
 	GameplayVars.current_rock_count = rock_count
 	GameplayVars.current_paper_count = paper_count
 	GameplayVars.current_scissors_count = scissors_count
-		
+	spawn_units()
+
 func spawn_units():
 	var rng = RandomNumberGenerator.new()
 	for i in range(0, rock_count):
-		var new_rock_unit = field_unit_scene.instantiate()
-		new_rock_unit.unit_type = 'rock'
-		new_rock_unit.field_units_group = %field_unit_container
-		new_rock_unit.position = Vector2(rng.randf_range(rand_rock_x_min, rand_rock_x_max), rng.randf_range(rand_rock_y_min, rand_rock_y_max))
-		new_rock_unit.field_unit_type_update.connect(_on_field_unit_type_update)
-		%field_unit_container.call_deferred("add_child", new_rock_unit)
+		create_field_unit('rock', Vector2(rng.randf_range(rand_rock_x_min, rand_rock_x_max), rng.randf_range(rand_rock_y_min, rand_rock_y_max)))
 	for i in range(0, scissors_count):
-		var new_scissors_unit = field_unit_scene.instantiate()
-		new_scissors_unit.unit_type = 'scissors'
-		new_scissors_unit.field_units_group = %field_unit_container
-		new_scissors_unit.position = Vector2(rng.randf_range(rand_scissors_x_min, rand_scissors_x_max), rng.randf_range(rand_scissor_y_min, rand_scissor_y_max))
-		new_scissors_unit.field_unit_type_update.connect(_on_field_unit_type_update)
-		%field_unit_container.call_deferred("add_child", new_scissors_unit)
+		create_field_unit('scissors', Vector2(rng.randf_range(rand_scissors_x_min, rand_scissors_x_max), rng.randf_range(rand_scissor_y_min, rand_scissor_y_max)))
 	for i in range(0, paper_count):
-		var new_paper_unit = field_unit_scene.instantiate()
-		new_paper_unit.unit_type = 'paper'
-		new_paper_unit.field_units_group = %field_unit_container
-		new_paper_unit.position = Vector2(rng.randf_range(rand_paper_x_min, rand_paper_x_max), rng.randf_range(rand_paper_y_min, rand_paper_y_max))
-		new_paper_unit.field_unit_type_update.connect(_on_field_unit_type_update)
-		%field_unit_container.call_deferred("add_child", new_paper_unit)
+		create_field_unit('paper', Vector2(rng.randf_range(rand_paper_x_min, rand_paper_x_max), rng.randf_range(rand_paper_y_min, rand_paper_y_max)))
+
+func create_field_unit(unit_type: String, pos: Vector2):
+		var new_field_unit = field_unit_scene.instantiate()
+		new_field_unit.unit_type = unit_type
+		new_field_unit.field_units_group = %field_unit_container
+		new_field_unit.position = pos
+		new_field_unit.field_unit_type_update.connect(_on_field_unit_type_update)
+		%field_unit_container.call_deferred("add_child", new_field_unit)
 		
 func _on_field_unit_type_update():
 	if GameplayVars.current_scissors_count == total_units_count:
