4367628b4e671cf6f5dceee001c7541c57e30323
jjwall <jacob.wallace@utexas.edu>
2023-07-26 23:32:57 -0500

    line timer before tween animation starts and some func org

--- objects/player_cursor/player_cursor.gd
+++ objects/player_cursor/player_cursor.gd
@@ -11,16 +11,14 @@ var line_points = PackedVector2Array()
 var collision_children: Array[CollisionShape2D] = []
 var line_node: Line2D
 var tween: Tween
-
+var line_is_on_screen = false
 
 # TODO: Temporary ink lines (fade to red over time)
 
 func _ready():
 	# Get the reference to the Line2D and CollisionPolygon2D nodes
 	line_node = get_node("Line2D")
-	tween = get_tree().create_tween()
-	tween.tween_property(line_node, "modulate", Color.RED, 1)
-	tween.pause()
+	set_tween()
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -38,24 +36,34 @@ func _input(event):
 				self.add_child(shape)
 				collision_children.append(shape)
 				draw_ink.emit(collision_line.a.distance_to(collision_line.b))
-#				line_node.default_color = Color(1,1,0,0)
-#				line_node.set
-				tween.play()
+				init_line()
 				
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		# Reset the line_points array when the left mouse button is pressed
 		reset_ink_meter.emit()
 		line_points.clear()
 		line_node.points = line_points
-		line_node.modulate = Color(1, 1, 1, 1)
-		tween.kill()
-		tween = get_tree().create_tween()
-		tween.tween_property(line_node, "modulate", Color.RED, 1)
-		tween.pause()
+		reset_tween()
 		delete_collision_children()
-		
+		line_is_on_screen = false
+
+func init_line():
+	if !line_is_on_screen:
+		line_is_on_screen = true
+#		set_tween()
+		$init_line_timer.start()
+
+func set_tween():
+	tween = get_tree().create_tween()
+	tween.tween_property(line_node, "modulate", Color.RED, 1)
+	tween.pause()
+
 func reset_tween():
-	pass
+	line_node.modulate = Color(1, 1, 1, 1)
+	tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(line_node, "modulate", Color.RED, 1)
+	tween.pause()
 
 func delete_collision_children():
 	for n in range(collision_children.size()):
@@ -63,3 +71,6 @@ func delete_collision_children():
 		
 	collision_children = []
 
+func _on_timer_timeout():
+	if line_is_on_screen:
+		tween.play()
