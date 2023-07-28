f627f46b5dabb4d101de6450440e2363e924f4ab
jjwall <jacob.wallace@utexas.edu>
2023-07-27 22:36:11 -0500

    mission buttons aligned and added text

--- scenes/mission_menu/mission_menu.gd
+++ scenes/mission_menu/mission_menu.gd
@@ -3,42 +3,42 @@ extends Control
 var gameplay_scene = "res://scenes/gameplay/gameplay.tscn"
 var main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
 var ui_theme = preload("res://assets/themes/ui_theme.tres")
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	create_mission_buttons()
 	$mission_01.visible = false
 
 func create_mission_buttons():
-	var button_pos_y = -150
-	var button_pos_x = 100
-	var button_height = 200
-	var button_length = 200
+	var button_pos_y = -75
+	var button_pos_x = 65
+	var mission_number = 0
+	
 	for r in range(7):
 		button_pos_y += 250
-		button_pos_x = 100
-		print(r)
-		for c in range(4):
-			var new_mission_button = Button.new()
-			new_mission_button.set_position(Vector2(button_pos_x, button_pos_y))
-			new_mission_button.set_size(Vector2(button_length, button_height))
-			new_mission_button.theme = ui_theme
-			self.add_child(new_mission_button)
-			button_pos_x += 250
-			print(c)
+		button_pos_x = 65
 		
-#		function buildGrid (map) {
-#		var grid =$("#grid");
-#		grid.empty();
-#		for (var r = 0; r < map.length; r++) {
-#			for (var c = 0; c < map[0].length; c++) {
-#				if (map[r][c]) {
-#					//$(`#grid .grid-square[data-r=${r}][data-c=${c}]`).append(`<img src=${map[r][c]}>`);
-#					grid.append(`<div class="grid-square" data-r = "${r}" data-c = "${c}" data-action=${map[r][c].action}><div class="grid-style" data-r = "${r}" data-c = "${c}"></div><img src=${map[r][c].img}></div>`);
-#				} else {
-#					grid.append(`<div class="grid-square" data-r = "${r}" data-c = "${c}" data-action="0"><div class="grid-style" data-r = "${r}" data-c = "${c}"></div></div>`);
-#				}
-#			}
-#			grid.append("<br>");
+		for c in range(4):
+			var button_text = ""
+			mission_number += 1
+			
+			if mission_number < 10:
+				button_text += "0"
+				button_text += str(mission_number)
+			else:
+				button_text += str(mission_number)
+				
+			create_mission_button(Vector2(button_pos_x, button_pos_y), button_text)
+			button_pos_x += 250
+
+func create_mission_button(pos: Vector2, text: String):
+	var button_height = 200
+	var button_length = 200
+	var new_mission_button = Button.new()
+	new_mission_button.set_position(pos)
+	new_mission_button.set_size(Vector2(button_length, button_height))
+	new_mission_button.theme = ui_theme
+	new_mission_button.text = text
+	self.add_child(new_mission_button)
 
 func change_to_scene(scene: String):
 	get_tree().change_scene_to_file(scene)
