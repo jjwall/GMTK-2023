ace9f552a502e81c101c97cd046a1c124211f4b0
jjwall <jacob.wallace@utexas.edu>
2023-07-27 23:33:37 -0500

    added data store global var (mocking data for now). Print mission meta data on click

---
+++ globals/data_store.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# Mocking save game functionality for now.
+var missions := {
+	"01" = {
+		"locked": false,
+		"stars": 0,
+	},
+	"02" = {
+		"locked": true,
+		"stars": 0,
+	},
+	"03" = {
+		"locked": true,
+		"stars": 0,
+	},
+	"04" = {
+		"locked": true,
+		"stars": 0,
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scenes/mission_menu/mission_menu.gd
+++ scenes/mission_menu/mission_menu.gd
@@ -18,31 +18,34 @@ func create_mission_buttons():
 		button_pos_x = 65
 		
 		for c in range(4):
-			var button_text = ""
+			var mission_id = ""
 			mission_number += 1
 			
 			if mission_number < 10:
-				button_text += "0"
-				button_text += str(mission_number)
+				mission_id += "0"
+				mission_id += str(mission_number)
 			else:
-				button_text += str(mission_number)
+				mission_id += str(mission_number)
 				
-			create_mission_button(Vector2(button_pos_x, button_pos_y), button_text)
+			create_mission_button(Vector2(button_pos_x, button_pos_y), mission_id)
 			button_pos_x += 250
 
-func create_mission_button(pos: Vector2, text: String):
+func create_mission_button(pos: Vector2, mission_id: String):
 	var button_height = 200
 	var button_length = 200
 	var new_mission_button = Button.new()
 	new_mission_button.set_position(pos)
 	new_mission_button.set_size(Vector2(button_length, button_height))
 	new_mission_button.theme = ui_theme
-	new_mission_button.text = text
-	new_mission_button.pressed.connect(on_mission_button_pressed.bind(text))
+	new_mission_button.text = mission_id
+	new_mission_button.pressed.connect(on_mission_button_pressed.bind(mission_id))
 	self.add_child(new_mission_button)
 
-func on_mission_button_pressed(mission_text: String):
-	print(mission_text)
+func on_mission_button_pressed(mission_id: String):
+	if DataStore.missions.has(mission_id):
+		print(DataStore.missions[mission_id])
+	else:
+		print("Mission %s is not in data store" % mission_id)
 
 func change_to_scene(scene: String):
 	get_tree().change_scene_to_file(scene)
