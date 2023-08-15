2bcf1bdfec7ca9801e7f3d7cc54cab8a693c8aea
jjwall <jacob.wallace@utexas.edu>
2023-08-15 00:33:10 -0500

    delete data confirmation banner

--- scenes/options_menu/options_menu.gd
+++ scenes/options_menu/options_menu.gd
@@ -1,9 +1,16 @@
+# TODO: Credits
+# TODO: Implement sound volume changing
+# TODO: Save volume slider values on change
+# TODO: (Done) Data deleted banner
+# TODO: Implement No Ad purchase
+
 extends Control
 
 const main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
 
 func _ready():
 	$delete_data_modal.visible = false
+	$data_deleted_banner.modulate.a = 0
 	$sfx_volume_slider.value = DataStore.current.sfx_volume
 	$music_volume_slider.value = DataStore.current.music_volume
 
@@ -17,6 +24,17 @@ func _on_delete_button_pressed():
 	DataStore.delete_data()
 	$sfx_volume_slider.value = DataStore.current.sfx_volume
 	$music_volume_slider.value = DataStore.current.music_volume
+	$delete_data_modal.visible = false
+	$data_deleted_banner.modulate.a = 1
+	$banner_timer.start()
 
 func _on_cancel_button_pressed():
 	$delete_data_modal.visible = false
+
+func _on_banner_timer_timeout():
+	fade_banner_out()
+
+func fade_banner_out():
+	# Note: Tween doesn't reset so banner only shows up once.
+	var tween = create_tween()
+	tween.tween_property($data_deleted_banner, "modulate:a", 0, 1)
