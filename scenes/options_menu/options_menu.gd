56f28cddca9b195bc3824fc455fdeabe7ef8dab7
jjwall <jacob.wallace@utexas.edu>
2023-08-15 00:45:45 -0500

    sfx and music volume value data saves in local store

--- scenes/options_menu/options_menu.gd
+++ scenes/options_menu/options_menu.gd
@@ -1,8 +1,9 @@
 # TODO: Credits
 # TODO: Implement sound volume changing
-# TODO: Save volume slider values on change
+# TODO: (Done) Save volume slider values on change
 # TODO: (Done) Data deleted banner
 # TODO: Implement No Ad purchase
+# TODO: Restore Purchases for iOS
 
 extends Control
 
@@ -10,6 +11,7 @@ const main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
 
 func _ready():
 	$delete_data_modal.visible = false
+	$data_deleted_banner.visible = false
 	$data_deleted_banner.modulate.a = 0
 	$sfx_volume_slider.value = DataStore.current.sfx_volume
 	$music_volume_slider.value = DataStore.current.music_volume
@@ -25,6 +27,7 @@ func _on_delete_button_pressed():
 	$sfx_volume_slider.value = DataStore.current.sfx_volume
 	$music_volume_slider.value = DataStore.current.music_volume
 	$delete_data_modal.visible = false
+	$data_deleted_banner.visible = true
 	$data_deleted_banner.modulate.a = 1
 	$banner_timer.start()
 
@@ -38,3 +41,15 @@ func fade_banner_out():
 	# Note: Tween doesn't reset so banner only shows up once.
 	var tween = create_tween()
 	tween.tween_property($data_deleted_banner, "modulate:a", 0, 1)
+	tween.tween_callback(on_banner_fade_end)
+	
+func on_banner_fade_end():
+	$data_deleted_banner.visible = false
+
+func _on_music_volume_slider_drag_ended(value_changed):
+	DataStore.current.music_volume = $music_volume_slider.value
+	DataStore.save()
+
+func _on_sfx_volume_slider_drag_ended(value_changed):
+	DataStore.current.sfx_volume = $sfx_volume_slider.value
+	DataStore.save()
