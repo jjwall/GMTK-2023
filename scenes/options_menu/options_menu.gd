4c7df9e1e1907b2f1cbaaf9cb9b5121679bb2c4a
jjwall <jacob.wallace@utexas.edu>
2023-08-15 01:00:27 -0500

    credits modal

--- scenes/options_menu/options_menu.gd
+++ scenes/options_menu/options_menu.gd
@@ -1,15 +1,17 @@
-# TODO: Credits
+# TODO: (Done) Credits
 # TODO: Implement sound volume changing
 # TODO: (Done) Save volume slider values on change
 # TODO: (Done) Data deleted banner
 # TODO: Implement No Ad purchase
 # TODO: Restore Purchases for iOS
+# TODO: Dev unlock all levels
 
 extends Control
 
 const main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
 
 func _ready():
+	$credits_modal.visible = false
 	$delete_data_modal.visible = false
 	$data_deleted_banner.visible = false
 	$data_deleted_banner.modulate.a = 0
@@ -46,10 +48,16 @@ func fade_banner_out():
 func on_banner_fade_end():
 	$data_deleted_banner.visible = false
 
-func _on_music_volume_slider_drag_ended(value_changed):
+func _on_music_volume_slider_drag_ended(_value_changed):
 	DataStore.current.music_volume = $music_volume_slider.value
 	DataStore.save()
 
-func _on_sfx_volume_slider_drag_ended(value_changed):
+func _on_sfx_volume_slider_drag_ended(_value_changed):
 	DataStore.current.sfx_volume = $sfx_volume_slider.value
 	DataStore.save()
+
+func _on_credits_button_pressed():
+	$credits_modal.visible = true
+
+func _on_credits_back_button_pressed():
+	$credits_modal.visible = false
