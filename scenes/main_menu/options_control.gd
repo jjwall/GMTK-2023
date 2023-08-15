e466f63875f8ee173514773e345205c0b599f421
jjwall <jacob.wallace@utexas.edu>
2023-08-14 22:28:09 -0500

    base options menu screen scene switchig

--- scenes/main_menu/options_control.gd
+++ scenes/main_menu/options_control.gd
@@ -1,9 +1,11 @@
 extends Control
-#
+
+const options_menu_scene = "res://scenes/options_menu/options_menu.tscn"
+
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			open_options()
 	
 func open_options():
-	print("scene switch to options")
+	SceneSwitcher.change_to_scene(options_menu_scene)
