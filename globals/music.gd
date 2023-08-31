fb9f968bc6d98cb5a3a14f89cf14208476e45745
jjwall <jacob.wallace@utexas.edu>
2023-08-31 18:19:02 -0500

    new music added

--- globals/music.gd
+++ globals/music.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 func PlayMainMusic():
-	if $Green_Pastures.playing:
+	if $Rock_Paper_Showdown_Main.playing:
 		return
 #	$Gravity_Way.stop()
-	$Green_Pastures.play()
+	$Rock_Paper_Showdown_Main.play()
 	print("Play Main Theme")
 	
 #func PlayGameplayMusic():
