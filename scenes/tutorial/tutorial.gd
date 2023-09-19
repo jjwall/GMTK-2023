d21da2a151cc23ca18d185884b0504578632c7af
jjwall <jacob.wallace@utexas.edu>
2023-09-18 19:00:23 -0500

    tutorial screen updates

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -1,5 +1,8 @@
 extends Control
 
+# TODO: Panel for RPS grapics
+# TODO: Make RPS graphics bigger
+
 const main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
 
 func _on_back_button_pressed():
