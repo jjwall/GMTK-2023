871bc71493a8fb59f1ddd1b190309adb00f9f282
Jacob <jacob.wallace@utexas.edu>
2025-08-22 14:39:54 -0500

    deleting android build files b/c that shouldn't be committed to the repo

--- android/build/build/intermediates/assets/debug/mergeDebugAssets/globals/gameplay_vars.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var current_rock_count = 0
-var current_scissors_count = 0
-var current_paper_count = 0
-
-var ink_meter_value = 0
-
-var dev_unlock = false
--- android/build/build/intermediates/assets/debug/mergeDebugAssets/scenes/tutorial/tutorial.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# TODO: Panel for RPS grapics
-# TODO: Make RPS graphics bigger
-
-const main_menu_scene = "res://scenes/main_menu/main_menu.tscn"
-
-func _on_back_button_pressed():
-	SceneSwitcher.change_to_scene(main_menu_scene)
